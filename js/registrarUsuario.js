0c05aba953bdfb90b756f6080544c676050ee3f8
franciscogelabert <francisco.gelabert@gmail.com>
2023-07-27 15:16:18 -0300

    se agrega codigo de asincronia promocion

--- js/registrarUsuario.js
+++ js/registrarUsuario.js
@@ -1,10 +1,30 @@
+const btnIngresar = document.getElementById("ingresar");
+btnIngresar.addEventListener("click", abrirNuevoTab);
 const btnPublicar = document.getElementById("registrarse");
 btnPublicar.addEventListener("click", verificarDatos);
+let registro = false;
+
+
+let timeleft = 40;
+let tiempoPromo = setInterval(function () {
+
+  if (registro) {
+    clearInterval(tiempoPromo);
+    document.getElementById("reloj").innerHTML = "Su Usario ya participa por el sorteo de un 0 KM !!!!!!!";
+
+  } else if (timeleft <= 0) {
+    clearInterval(tiempoPromo);
+    document.getElementById("reloj").innerHTML = "Tiempo finalizado";
+  } else {
+    document.getElementById("reloj").innerHTML = "Quedan " + timeleft + " Segundos ";
+  }
+  timeleft -= 1;
+}, 1000);
 
 
 function verificarDatos() {
   if (validarForm()) {
-
+    registro = true;
     Swal.fire({
       title: 'Todos los datos son correctos?',
       showDenyButton: true,
@@ -18,7 +38,6 @@ function verificarDatos() {
         Swal.fire('Puede seguir editando sus datos', '', 'info')
       }
     })
-
 
   } else {
     Swal.fire({
@@ -48,4 +67,9 @@ function validarForm() {
   const validar = inputEmail.value != '' && inputPassword.value != '' && userName.value != '' && fechaNac.value != '' && idgenero.value != '';
   return validar;
 
+}
+
+
+function abrirNuevoTab() {
+  window.open('404.html');
 }
